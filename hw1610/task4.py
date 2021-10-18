4020c1d2c58e44ee9531387deccd69f595a24906
externalcharm <freesxsad23@mail.ru>
2021-10-18 21:55:33 +0300

    homework1610

--- hw1610/task4.py
+++ hw1610/task4.py
@@ -1,4 +1,8 @@
-text = input("Введите первую строку стихотворения: ")
-print(len(text))
-index = input("")
-print(index[::])
+firstString = input("Введите первую строку стихотворения: ")
+length = len(firstString)
+print("Длина строки: ", length)
+start = int(input("Введите начальную позицию: "))
+end = int(input("Введите конечную позицию: "))
+part = (firstString[start:end])
+print(part)
+
