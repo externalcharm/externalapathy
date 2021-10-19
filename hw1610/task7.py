9766bdb5e8fc1163bdcee0a750c0be2faaf75217
externalcharm <freesxsad23@mail.ru>
2021-10-19 09:31:56 +0300

    homework1610

---
+++ hw1610/task7.py
@@ -0,0 +1,24 @@
+word = input("Введите слово для перевода: ")
+vowles = ['a', 'e', 'u', 'y', 'i', 'o']
+
+
+def translate(word):
+    global lindex
+    first = word[0]
+    if first in vowles:
+        word = word.lower()
+        word += "way"
+        return word
+    else:
+        word = word.lower()
+
+        for letter in word:
+            if letter in vowles:
+                lindex = word.index(letter)
+                break
+
+        word = word[lindex:] + word[:lindex] + "ay"
+        return word
+
+
+print(translate(word))
