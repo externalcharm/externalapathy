5009cb47abee0049ddadc2cd9877b2803c679cd3
externalcharm <freesxsad23@mail.ru>
2021-10-25 17:38:07 +0300

    homework2310

--- hw2310/task7.py
+++ hw2310/task7.py
@@ -1,11 +1,10 @@
-ThreeDigNumbers = ["000", "111", "222", "333"]
-while True:
+ThreeDigNumbers = [111, 222, 333, 444]
 
-    for num in ThreeDigNumbers:
-        print(num)
+for num in ThreeDigNumbers:
+    print(num)
 
-    usernum = int(input("Введите число из трех цифр из списка: "))
-    if usernum == ThreeDigNumbers:
-        print(ThreeDigNumbers[usernum])
-# else:
-#     print("That is not in the list")
+usernum = int(input("Введите число из трех цифр из списка: "))
+if usernum in ThreeDigNumbers:
+    print(ThreeDigNumbers.index(usernum))
+else:
+     print("That is not in the list")
