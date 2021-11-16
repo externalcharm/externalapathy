6276bfaa1a337a4f7c1a1e3be96d4b1f36b9c7c9
externalcharm <freesxsad23@mail.ru>
2021-11-16 17:37:29 +0300

    homework1311

---
+++ hw1311/task1.py
@@ -0,0 +1,59 @@
+from random import randint
+from random import choice
+
+
+class Door:
+    def __init__(self, instance):
+        self.instance = instance
+
+    def openDoor(self, hero):
+        if isinstance(self.instance, Artefacts):
+            hero.health += self.instance.force
+        else:
+            hero.health -= self.instance.damage
+
+
+class Artefacts:
+    def __init__(self):
+        self.force = randint(10, 80)
+
+    def __str__(self):
+        return "Артефакт с " + str(self.force) + " хп"
+
+
+class Monsters:
+    def __init__(self):
+        self.damage = randint(5, 100)
+
+    def __str__(self):
+        return "Монстр с " + str(self.damage) + " урона"
+
+
+class Hero:
+    def __init__(self, health):
+
+        self.health = health
+
+    def __str__(self):
+        return "Здоровье героя:" + str(self.health)
+
+
+# здоровье героя
+hero = Hero(25)
+# двери
+door = {}
+for i in range(10):
+    instances = [Artefacts(), Monsters()]  # переменная с артефактами и монстрами
+    door[i] = Door(choice(instances))
+# логика открывания дверей с выводом артефактов, монстров и здоровья героя
+for i in range(10):
+    door[i].openDoor(hero)
+# проверка хп героя
+    if hero.health > 1:
+        print(hero)
+        print(Artefacts(), Monsters())
+        continue
+    elif hero.health < 0:
+        print(hero, '\n' "Ваш герой был убит, вы проиграли")
+        print("За дверью был", Artefacts(), "и", Monsters())
+        break
