05543c66ddc47d0addbe151941570ee1b2294973
externalcharm <freesxsad23@mail.ru>
2022-02-09 01:02:21 +0300

    homework2310

--- hw2310/task1.py
+++ hw2310/task1.py
@@ -1,5 +1,4 @@
 tuple1 = ("Belarus", "USA", "Africa", "Russia", "Nederlands")
-for country in tuple1:
-    print(country)
-    userch = input("Введите название страны: ").capitalize()
-    print(tuple1.index(userch))
+print(tuple1)
+userch = input("Введите название страны: ").capitalize()
+print(tuple1.index(userch))
